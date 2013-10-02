9d7660a83a0f02176e2fa05bcac2270bb34b871e
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-10-02 13:28:00 -0400

    add doc; format code

--- bin/ex04.dart
+++ bin/ex04.dart
@@ -1,62 +1,19 @@
 import 'dart:math' as mathematique;
-  void main (){
 
-    // Source: https://github.com/dzenanr/ondart_examples/blob/master/ondart_functions/fibonacci_with_iteration.dart
-
-    print('Question 1:'
-      );
-
-  String fibonacciIteration(var t) {
-    var a = 0, b = 1, f = 1, n = 1;
-    String result = '0' ' 1';
-    while (n < t) {
-      f = a + b;
-      result = '$result $f';
-      a = b;
-      b = f;
-      n = n + 1;
-    }
-    return result;
+String fibonacciIteration(var t) {
+  var a = 0, b = 1, f = 1, n = 1;
+  String result = '0' ' 1';
+  while (n < t) {
+    f = a + b;
+    result = '$result $f';
+    a = b;
+    b = f;
+    n = n + 1;
   }
-  int count = 10;
-  String sequence =
-      fibonacciIteration(count);
-  print('Fibonacci: '
-        '${count + 1} nombres dans la sequence');
-  print(sequence);
+  return result;
+}
 
-print('Question 2:');
-var width = 980;
-var height = 660;
-var yline = (height / 2) + (mathematique.sin(33*mathematique.PI/180)*120);
-var xline= (width / 2) + (mathematique.cos(33*mathematique.PI/180)*120);
-print('Debut ligne:(${width / 2}, ${height / 2})');
-print('Fin ligne:(${xline.toStringAsFixed(2)} , ${yline.toStringAsFixed(2)})');
-
-print('Question 3:');
-
- var jours = 365;
-  DateTime mafete = new DateTime(1990,11,01,12,0,0);
-  DateTime now = new DateTime.now();
-  Duration ecart = now.difference(mafete);
-  print("Voici le temps ecoule depuis le moment de ma naissance");
-print('En jour: ${ecart.inDays}');
-print('En heures: ${ecart.inHours}');
-print('En minute: ${ecart.inMinutes}');
-print('En seconde: ${ecart.inSeconds}');
-print ('En annee: ${(ecart.inDays)/jours}');
-
-print('Question 4:');
-
-var interest=0.033;
-var invest=1000;
-var taux = (1+interest);
-  for (int o = 1; o < 11; o++)
-     print(invest*mathematique.pow(taux,o)-invest);
-
-print('Question 5:');
-
-String table (int t){
+String table (int t) {
   int a = 2, c = 1, b = 12, x = 0, y = 0;
   String resultat = "12";
   while (x < t) {
@@ -67,11 +24,52 @@ String table (int t){
   }
   return resultat;
 }
-     int compter = 19;
-     String total =
-         table (compter);
-     print('Les '
-         '${compter + 1} premiers resultats de la table de 12:');
-     print(total);
+
+void main (){
+  // Source: https://github.com/dzenanr/ondart_examples/blob/master/ondart_functions/fibonacci_with_iteration.dart
+  print('Question 1:');
+
+  int count = 10;
+  String sequence =
+      fibonacciIteration(count);
+  print('Fibonacci: '
+        '${count + 1} nombres dans la sequence');
+  print(sequence);
+
+  print('Question 2:');
+  
+  var width = 980;
+  var height = 660;
+  var yline = (height / 2) + (mathematique.sin(33*mathematique.PI/180)*120);
+  var xline= (width / 2) + (mathematique.cos(33*mathematique.PI/180)*120);
+  print('Debut ligne:(${width / 2}, ${height / 2})');
+  print('Fin ligne:(${xline.toStringAsFixed(2)} , ${yline.toStringAsFixed(2)})');
+
+  print('Question 3:');
+
+  var jours = 365;
+  DateTime mafete = new DateTime(1990,11,01,12,0,0);
+  DateTime now = new DateTime.now();
+  Duration ecart = now.difference(mafete);
+  print("Voici le temps ecoule depuis le moment de ma naissance");
+  print('En jour: ${ecart.inDays}');
+  print('En heures: ${ecart.inHours}');
+  print('En minute: ${ecart.inMinutes}');
+  print('En seconde: ${ecart.inSeconds}');
+  print ('En annee: ${(ecart.inDays)/jours}');
+  
+  print('Question 4:');
+  
+  var interest=0.033;
+  var invest=1000;
+  var taux = (1+interest);
+  for (int o = 1; o < 11; o++) print(invest*mathematique.pow(taux,o)-invest);
+
+  print('Question 5:');
+
+  int compter = 19;
+  String total = table(compter);
+  print('Les ${compter + 1} premiers resultats de la table de 12:');
+  print(total);
 }
 
